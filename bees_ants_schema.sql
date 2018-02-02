12da107f81364b11120f5fa84c277bdac2c5ec5e
Dan Connolly <dckc@madmode.com>
2018-02-02 02:17:54 -0600

    pay periods, github_users, money

--- bees_ants_schema.sql
+++ bees_ants_schema.sql
@@ -6,72 +6,95 @@ create table issue (
 )
 ;
 
-drop table if exists person;
-create table person (
-  gh varchar(64) primary key
-)
-;
+drop table if exists pay_period;
+
+create table pay_period (
+        start_date date primary key,
+        end_date date not null
+        )
+    ;
+
+drop table if exists admin_settings;
+create table admin_settings (
+        id integer primary key auto_increment,
+        current_pay_period date not null,
+        foreign key fk_admin_period(current_pay_period)
+        references pay_period(start_date)
+        )
+    ;
 
 -- TODO: one vote per voter per issue?
 drop table if exists budget_vote;
+
 create table budget_vote (
   id integer primary key auto_increment,
   issue_num integer not null,
-  amount integer not null,  -- USD * 100
-  voter_gh varchar(64) not null,  -- github username
+  amount decimal(13, 2) not null,  -- USD * 100
+  pay_period date not null,
+  voter varchar(64) not null,
   vote_time timestamp,
-  foreign key fk_issue(issue_num) references issue(num),
-  foreign key fk_voter(voter_gh) references person(gh)
+  foreign key fk_budget_period(pay_period) references pay_period(start_date),
+  foreign key fk_budget_issue(issue_num) references issue(num),
+  foreign key fk_budget_voter(voter) references github_users(login)
 )
 ;
 
 -- TODO: one vote per voter per issue x worker?
 drop table if exists reward_vote;
+
 create table reward_vote (
   id integer primary key auto_increment,
   issue_num integer not null,
   percent integer not null,  -- TODO: constrain betwen 1 and 100
-  worker_gh varchar(64) not null,
-  voter_gh varchar(64) not null,
-  vote_time timestamp,
-  foreign key fk_issue(issue_num) references issue(num),
-  foreign key fk_voter(voter_gh) references person(gh),
-  foreign key fk_worker(worker_gh) references person(gh)
+  worker varchar(64) not null,
+  pay_period date not null,
+  voter varchar(64) not null,
+  vote_time timestamp not null,
+  foreign key fk_reward_period(pay_period) references pay_period(start_date),
+  foreign key fk_reward_issue(issue_num) references issue(num),
+  foreign key fk_reward_voter(voter) references github_users(login),
+  foreign key fk_reward_worker(worker) references github_users(login),
+  check (percent between 0 and 100)
 )
 ;
 
 
 -- TODO: latest vote only.
 create or replace view issue_budget as
-select issue_num, title, voter_qty, voters, amount_avg
+select pay_period, issue_num, title, voter_qty, voters, amount_avg
      , case when voter_qty >= 3 then amount_avg else null end amount_effective
 from (
 	select bv.issue_num, i.title
-	     , count(distinct bv.voter_gh) voter_qty
-	     , group_concat(bv.voter_gh separator ', ') voters
-	     , avg(bv.amount) amount_avg
+	     , count(distinct bv.voter) voter_qty
+	     , group_concat(bv.voter separator ', ') voters
+	     , round(avg(bv.amount), 2) amount_avg
+             , bv.pay_period
 	from issue i
-	join budget_vote bv on bv.issue_num = i.num
+	    join budget_vote bv on bv.issue_num = i.num
+            join admin_settings s on s.current_pay_period = bv.pay_period
 	group by i.num, i.title
 ) ea
 ;
 -- select * from issue_budget;
 
 create or replace view reward as
-select worker_gh, issue_num, title
+select pay_period, worker, issue_num, title
      , voter_qty, voters
+     , budget_avg, budget_effective
      , percent_avg
-     , amount_avg
-     , case when voter_qty >= 3 then amount_effective else null end amount_effective
+     , reward_avg
+     , case when voter_qty >= 3 then reward_effective else null end reward_effective
 from (
-	select ib.issue_num, ib.title, rv.worker_gh
-	     , count(distinct rv.voter_gh) voter_qty
-	     , group_concat(rv.voter_gh separator ', ') voters
-	     , avg(rv.percent) percent_avg
-	     , avg(rv.percent) / 100 * ib.amount_avg amount_avg
-	     , avg(rv.percent) / 100 * ib.amount_effective amount_effective
+	select ib.pay_period, ib.issue_num, ib.title, rv.worker
+	     , count(distinct rv.voter) voter_qty
+	     , group_concat(rv.voter separator ', ') voters
+             , ib.amount_avg budget_avg
+             , ib.amount_effective budget_effective
+	     , round(avg(rv.percent), 2) percent_avg
+	     , round(avg(rv.percent) / 100 * ib.amount_avg) reward_avg
+	     , round(avg(rv.percent) / 100 * ib.amount_effective) reward_effective
 	from issue_budget ib
-	join reward_vote rv on rv.issue_num = ib.issue_num
-	group by ib.issue_num, ib.title, rv.worker_gh
+	join reward_vote rv on rv.issue_num = ib.issue_num and rv.pay_period = ib.pay_period
+	group by ib.pay_period, ib.issue_num, ib.title, rv.worker
 ) ea
 ;
