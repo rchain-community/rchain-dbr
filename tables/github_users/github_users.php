724bf277d3c8177b130832fb72f11119be95463b
Me 261312 <261312@rchain-dbr.nfshost.com>
2018-02-02 22:54:26 +0000

    style tweaks: avatar, logo size

--- tables/github_users/github_users.php
+++ tables/github_users/github_users.php
@@ -4,7 +4,7 @@ class tables_github_users {
 		return $record->val('name').' '.$record->val('login');
 	}
 	function avatarUrl__htmlValue(&$record){
-		return '<img src="'.$record->val('avatarUrl').'" alt="'.$record->val('login').' width="20" height="20"/>';
+		return '<img src="'.$record->val('avatarUrl').'" alt="'.$record->val('login').' width="44" height="44"/>';
 	}
 }
 ?>
