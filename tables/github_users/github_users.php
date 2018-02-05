397a8f748fca34ca3f84a0da24008b70c5698aa4
Me 261312 <261312@rchain-dbr.nfshost.com>
2018-02-05 01:49:40 +0000

    link user locations to wikipedia

--- tables/github_users/github_users.php
+++ tables/github_users/github_users.php
@@ -6,5 +6,9 @@ class tables_github_users {
 	function avatarUrl__htmlValue(&$record){
 		return '<img src="'.$record->val('avatarUrl').'" alt="'.$record->val('login').' width="44" height="44"/>';
 	}
+
+	function location__htmlValue(&$record){
+		return '<a href="https://en.wikipedia.org/wiki/'.htmlspecialchars($record->val('location')).'" >'.$record->val('location').'</a>';
+	}
 }
 ?>
