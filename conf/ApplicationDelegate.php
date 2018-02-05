36c1a7a544c95b489ad783ea6e2fd111171afa07
Me 261312 <261312@rchain-dbr.nfshost.com>
2018-02-05 01:34:31 +0000

    constrain voter, pay period

--- conf/ApplicationDelegate.php
+++ conf/ApplicationDelegate.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * A delegate class for the entire application to handle custom handling of 
+ * A delegate class for the entire application to handle custom handling of
  * some functions such as permissions and preferences.
  */
 class conf_ApplicationDelegate {
     /**
-     * Returns permissions array.  This method is called every time an action is 
+     * Returns permissions array.  This method is called every time an action is
      * performed to make sure that the user has permission to perform the action.
      * @param record A Dataface_Record object (may be null) against which we check
      *               permissions.
@@ -16,24 +16,55 @@ class conf_ApplicationDelegate {
          $auth =& Dataface_AuthenticationTool::getInstance();
          $user =& $auth->getLoggedInUser();
 
-         if ( !isset($user) ) return Dataface_PermissionsTool::READ_ONLY();
+         $perms = Dataface_PermissionsTool::READ_ONLY();
+         if ( !isset($user) ) return $perms;
 
          $role = $user->val('permission');
 
-         // map github-speak to xataface-speak
-         // ISSUE: why doesn't permissions.ini EXTENDS work?
-         // cf. http://xataface.com/wiki/permissions.ini_file
-         if ($role == 'WRITE') {
-             $role = 'DELETE';
+         // TODO: managers
+
+         $app =& Dataface_Application::getInstance();
+         $query =& $app->getQuery();
+         $action = $query['-action'];
+
+         // OK to edit your own stuff
+         if ( $record ) {
+             $login = $user->val('login');
+             if ( $login == $record->getValue('voter') and
+                  ($action == 'view_related_record'  or
+                   $action == 'edit') ) {
+                 $perms['edit'] = 1;
+             }
+
+             else if ($query['-table'] == 'github_users' and
+                      $login == $record->getValue('login') and
+                      ($action == 'related_records_list' or
+                       $action == 'new_related_record') ) {
+                 $perms['add new related record'] = 1;
+             }
          }
 
-         // TODO: more managers
-         if ($user->val('login') == 'dckc') {
-             $role = 'MANAGER';
+         if ( in_array($query['-relationship'],
+                       array('BudgetVotes', 'RewardVotes')) ) {
+             $in_listing = $query['-action'] == 'related_records_list';
+             $adding = $query['-action'] == 'new_related_record';
+
+             // OK to add vote to issue, budget, or reward
+             if ( in_array($query['-table'], array('issue', 'issue_budget', 'reward')) ) {
+                 // TODO: $perms['copy'] = 1;
+
+                 if ( $in_listing or $adding ) {
+                     $perms['add new related record'] = 1;
+                 }
+                 else if ($viewing and
+                          $app->getRecord()->val('voter') == $user->val('login') ) {
+                     $perms['edit'] = 1;
+                 }
+             }
+
          }
 
-         return Dataface_PermissionsTool::getRolePermissions($role);
-             // Returns all of the permissions for the user's current role.
+         return $perms;
      }
 
     public function beforeHandleRequest(){
@@ -57,5 +88,37 @@ class conf_ApplicationDelegate {
         echo "<li><a href='https://github.com/rchain/Members/blob/master/CONTRIBUTING.md'>RAM Guide</a></li>\n";
         echo "</ul>\n";
     }
+}
+
+class VoteAuth {
+     function voter__default() {
+         $auth =& Dataface_AuthenticationTool::getInstance();
+         $user =& $auth->getLoggedInUser();
+         return $user->val('login');
+     }
+
+    function voter__permissions(&$record) {
+        return view_only();
+    }
+}
+
+class PayPeriodVote extends VoteAuth {
+    function pay_period__default () {
+        $res = df_query("select current_pay_period from admin_settings", null, true);
+        if ( !$res ) throw new Exception(mysql_error(df_db()));
+        $pp = $res[0]['current_pay_period'];
+        return $pp;
+    }
+
+    function pay_period__permissions(&$record) {
+        return view_only();
+    }
+}
+
+
+function view_only() {
+    $perms = Dataface_PermissionsTool::NO_ACCESS();
+    $perms['view']=1;
+    return $perms;
 }
 ?>
---
+++ tables/reward_vote/reward_vote.php
@@ -0,0 +1,3 @@
+<?php
+class tables_reward_vote extends PayPeriodVote {
+}
