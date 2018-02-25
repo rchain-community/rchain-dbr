d9d95c2075409f001cbb66bac1eb78d768d7cd16
Dan Connolly <dckc@madmode.com>
2018-02-25 02:32:06 +0000

    users: make websiteUrl a link

--- tables/github_users/github_users.php
+++ tables/github_users/github_users.php
@@ -1,14 +1,31 @@
 <?php
 class tables_github_users {
 	function getTitle(&$record){
-		return $record->val('name').' '.$record->val('login');
+        $login = $record->val('login');
+        $name = $record->val('name');
+        return strlen($name) > 0 ? ($name.' '.$login) : $login;
+	}
+	function login__htmlValue(&$record){
+        $login = $record->val('login');
+		return link_markup('https://github.com/'.$login, $login);
+	}
+	function websiteUrl__htmlValue(&$record){
+        $websiteUrl = $record->val('websiteUrl');
+		return strlen($websiteUrl) > 0 ? link_markup($websiteUrl, $websiteUrl) : '';
 	}
 	function avatarUrl__htmlValue(&$record){
-		return '<img src="'.$record->val('avatarUrl').'" alt="'.$record->val('login').' width="44" height="44"/>';
+        $login = $record->val('login');
+        $img = '<img src="'.$record->val('avatarUrl').'" alt="'.$login.' width="44" height="44"/>';
+		return link_markup('https://github.com/'.$login, $img);
 	}
-
 	function location__htmlValue(&$record){
-		return '<a href="https://en.wikipedia.org/wiki/'.htmlspecialchars($record->val('location')).'" >'.$record->val('location').'</a>';
+        $location = $record->val('location');
+		return link_markup('https://en.wikipedia.org/wiki/'.$location, $location);
 	}
 }
+
+function link_markup($href, $content) {
+		return '<a href="'.htmlspecialchars($href).'" >'.$content.'</a>';
+}
+
 ?>
