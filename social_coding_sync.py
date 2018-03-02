2a00e0f300d86faf20b80f78c5959a939bafd3ce
Dan Connolly <dckc@madmode.com>
2018-03-02 17:48:06 -0600

    social_coding_sync: normalize reactions

--- social_coding_sync.py
+++ social_coding_sync.py
@@ -3,7 +3,8 @@
 ..
 
 Usage:
-  social_coding_sync [options] reactions
+  social_coding_sync [options] reactions_get
+  social_coding_sync [options] reactions_norm
 
 Options:
   --repo-rd=FILE  JSON file with repository read-access token
@@ -16,6 +17,7 @@ import json
 import logging
 
 from docopt import docopt
+import pandas as pd
 import pkg_resources as pkg
 
 log = logging.getLogger(__name__)
@@ -26,18 +28,34 @@ def main(argv, cwd, build_opener):
     opt = docopt(__doc__.split('..\n', 1)[1], argv=argv[1:])
     log.debug('opt: %s', opt)
 
-    with (cwd / opt['--repo-rd']).open() as cred_fp:
+    cache = cwd / opt['--cache']
+    if opt['reactions_get']:
+        reactions_get(cwd / opt['--repo-rd'], build_opener(),
+                      cache / 'reactions.json')
+    elif opt['reactions_norm']:
+        voter = 'dckc'  # @@KLUDGE
+        with (cache / 'reactions.json').open('r') as fp:
+            reaction_info = json.load(fp)
+        reactions = Reactions.as_df(reaction_info)
+        with (cache / 'reactions.pkl').open('wb') as pkl:
+            reactions.to_pickle(pkl)
+        log.info('%d rows in saved df', len(reactions))
+        log.info('reactions by %s:\n%s', voter,
+                 reactions[reactions.index.get_level_values(0) == voter])
+
+
+def reactions_get(cred_path, web_ua, dest):
+    with cred_path.open() as cred_fp:
         repoRd = json.load(cred_fp)['token']
-    qs = QuerySvc(build_opener(), repoRd)
+    qs = QuerySvc(web_ua, repoRd)
     login = qs.runQ('query { viewer { login }}')
     log.info(login)
 
-    reactions = qs.runQ(Reactions.query)
-    out = cwd / opt['--cache'] / 'reactions.json'
-    with out.open('w') as data_fp:
-        json.dump(reactions, data_fp)
+    reaction_info = qs.runQ(Reactions.query)
+    with dest.open('w') as data_fp:
+        json.dump(reaction_info, data_fp)
     log.info('%d reactions saved to %s',
-             len(reactions['repository']['issues']['nodes']), out)
+             len(reaction_info['repository']['issues']['nodes']), dest)
 
 
 class QuerySvc(object):
@@ -67,6 +85,24 @@ class QuerySvc(object):
 
 class Reactions(object):
     query = pkg.resource_string(__name__, 'reactions.graphql').decode('utf-8')
+
+    endorsements = ['HEART', 'HOORAY', 'LAUGH', 'THUMBS_UP']
+
+    @classmethod
+    def as_df(cls, reactions):
+        return pd.DataFrame([
+            dict(issue_num=issue['number'],
+                 # title=issue['title'],
+                 worker=comment['author']['login'],
+                 createdAt=comment['createdAt'],
+                 # content=reaction['content'],
+                 voter=reaction['user']['login'],
+            )
+            for issue in reactions['repository']['issues']['nodes']
+            for comment in issue['comments']['nodes']
+            for reaction in comment['reactions']['nodes']
+            if reaction['content'] in cls.endorsements
+        ]).set_index(['voter', 'issue_num', 'worker']).sort_index()
 
 
 if __name__ == '__main__':
