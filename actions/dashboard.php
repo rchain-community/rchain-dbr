be535e6eb965e29f203cf35fb2ea3853a3fa4c2d
Dan Connolly <dckc@madmode.com>
2018-03-10 10:51:25 -0600

    dashboard: handle missing admin_settings

--- actions/dashboard.php
+++ actions/dashboard.php
@@ -2,7 +2,9 @@
 class actions_dashboard {
     function handle(&$params){
         $status = df_get_record('admin_settings', array());
-        df_display(array('current_pay_period'=>$status->strval('current_pay_period')),
+        df_display(array(
+            'current_pay_period'=> $status ? $status->strval('current_pay_period')
+                                           : "MISSING ADMIN_SETTINGS"),
                    'dashboard.html');
     }
 }
