e1420985f878f52f51d6f8edf6dc0770269ce349
Dan Connolly <dckc@madmode.com>
2018-03-13 02:13:21 -0500

    social_coding_sync: use config.ini
      - fix users_insert: typo
      - add phantom `[root]` config section
        because python's config parser can't handle title outside a group
      - flail about a bit before discovering mysql column charsets

--- social_coding_sync.py
+++ social_coding_sync.py
@@ -43,8 +43,7 @@ def main(argv, cwd, build_opener, create_engine):
     def config():
         log.info('config: %s', opt['--config'])
         with (cwd / opt['--config']).open('r') as txt_in:
-            cp = SafeConfigParser()
-            cp.read(txt_in, opt['--config'])
+            cp = headless_config(txt_in, opt['--config'])
         return cp
 
     def db():
@@ -55,12 +54,12 @@ def main(argv, cwd, build_opener, create_engine):
     def tok():
         return config().get('github_repo', 'read_token')
 
-    def cache_open(filename, mode, what):
+    def cache_open(filename, mode, what, encoding=None):
         path = cwd / opt['--cache'] / filename
         log.info('%s %s to %s',
                  'Writing' if mode == 'w' else 'Reading',
                  what, path)
-        return path.open(mode=mode)
+        return path.open(mode=mode, encoding=encoding)
 
 
     if opt['issues_fetch']:
@@ -71,7 +70,7 @@ def main(argv, cwd, build_opener, create_engine):
             json.dump(issueInfo, fp)
 
     elif opt['issues_insert']:
-        with cache_open('issues.json', mode='r',
+        with cache_open('issues.json', mode='r', encoding='utf-8',
                         what='issueInfo') as fp:
             issuePages = json.load(fp)
         Issues.db_sync(db(), Issues.data(issuePages), 'issue')
@@ -84,20 +83,20 @@ def main(argv, cwd, build_opener, create_engine):
             json.dump(userInfo, fp)
 
     elif opt['users_insert']:
-        with cache_open('users.json', mode='r',
+        with cache_open('users.json', mode='r', encoding='utf-8',
                         what='userInfo') as fp:
             userPages = json.load(fp)
-        Collaborators.db_sync(db(), Collaborators.data(issuePages), 'github_users')
+        Collaborators.db_sync(db(), Collaborators.data(userPages), 'github_users')
 
     elif opt['reactions_get']:
         rs = Reactions(build_opener(), tok())
-        info = rs.fetch(dest=cache / 'reactions.json')
+        info = rs.fetch(dest=cwd / opt['--cache'] / 'reactions.json')
         log.info('%d reactions saved to %s',
                  len(info['repository']['issues']['nodes']), opt['--cache'])
 
     elif opt['trust_seed']:
         log.info('using cache %s to get saved reactions', opt['--cache'])
-        with (cache / 'reactions.json').open('r') as fp:
+        with cache_open('reactions.json', mode='r', what='reactions') as fp:
             reaction_info = json.load(fp)
         dbr = db()
         reactions = Reactions.normalize(reaction_info)
@@ -113,6 +112,13 @@ def main(argv, cwd, build_opener, create_engine):
         trusted.to_sql('authorities', if_exists='replace', con=dbr,
                        dtype=noblob(trusted))
         log.info('trusted.head():\n%s', trusted.head())
+
+
+def headless_config(fp, fname):
+    txt = '[root]\r\n' + fp.read()
+    cp = SafeConfigParser()
+    cp.read_string(txt, fname)
+    return cp
 
 
 class QuerySvc(object):
@@ -174,6 +180,7 @@ class QuerySvc(object):
 
     @classmethod
     def db_sync(cls, db, data, table):
+        log.info('db_sync cols: %s', data.dtypes)
         cols = ', '.join(data.columns.values)
         params = ', '.join(
             ('%%(%s)s' % name) for name in data.columns)
@@ -184,7 +191,7 @@ class QuerySvc(object):
         values (%(params)s)
         on duplicate key update
         %(assignments)s
-        ''' % dict(cols=cols, params=params, assignments=assignments)
+        ''' % dict(cols=cols, params=params, assignments=assignments, table=table)
         log.info('db_sync SQL: %s', sql)
         records = data.to_dict(orient='records')
         with db.begin() as trx:
@@ -250,7 +257,9 @@ class Collaborators(QuerySvc):
             columns=['login', 'followers', 'name', 'location', 'email',
                      'bio', 'websiteUrl', 'avatarUrl', 'permission', 'createdAt'])
         # df = df.set_index('login')
-        df.createdAt = pd.to_datetime(df.createdAt)
+        # df.createdAt = pd.to_datetime(df.createdAt)
+        df = df.fillna(value='')
+        df.createdAt = df.createdAt.str.replace('T', ' ').str.replace('Z', '')
         return df
 
 
