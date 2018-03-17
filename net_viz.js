2b3de56c39f8233dfad42075f7e1cbaad0726c47
Dan Connolly <dckc@madmode.com>
2018-03-17 22:34:45 +0000

    add color to social network viz

--- net_viz.js
+++ net_viz.js
@@ -1,4 +1,4 @@
-function makeSocialNetwork(makeXHR) {
+function makeSocialNetwork(makeXHR, container) {
     function fetch() {
 	return new Promise(function (resolve, reject) {
 	    const xhr = makeXHR();
@@ -22,8 +22,35 @@ function makeSocialNetwork(makeXHR) {
 	    xhr.send();
 	});
     }
-    
+
+
+    function draw(net) {
+	// colors get darker like karate belts
+	const colors = {
+	    '0': 'white',
+	    apprentice: 'green',
+	    journeyer: 'orange',
+	    master: 'black'
+	};
+
+	// create an array with nodes
+	const nodes = new vis.DataSet(
+	    net.nodes.map(
+		n => Object.assign(n, {color: colors[n.rating_label]})));
+
+	// create an array with edges
+	const edges = new vis.DataSet(net.edges);
+
+	var data = {
+	    nodes: nodes,
+	    edges: edges
+	};
+	var options = {};
+	var network = new vis.Network(container, data, options);
+    }
+
     return Object.freeze({
-	fetch: fetch
+	fetch: fetch,
+	draw: draw
     });
 }
