0ccb901de491b1ccf81c5d19d9e170b58de6df72
Dan Connolly <dckc@madmode.com>
2018-08-04 14:14:11 -0500

    server.js: apply ocap discipline
      - use docopt for defaults

--- s2r/server.js
+++ s2r/server.js
@@ -1,13 +1,31 @@
-const mysql = require('mysql');
+const docopt = require('docopt').docopt;
 const MySQLEvents = require('@rodrigogs/mysql-events');
 
-const program = async () => {
-  const connection = mysql.createConnection({
-      host: '127.0.0.1', port: 3506,
-      user: process.env.DBR_BINLOG_USERNAME,
-      password: process.env.DBR_BINLOG_PASSWORD,
-  });
+const usage = `
+Replicate MySql events in RChain
 
+Usage:
+  server.js [options]
+
+Options:
+ --host HOST     mysql connection host [default: 127.0.0.1]
+ --port PORT     mysql connection port [default: 3506]
+ --user USER     mysql connection user [default: rchain_binlog]
+ --passkey VAR   environment variable for mysql password
+                 [default: RCHAIN_BINLOG_PASSWORD]
+ --help          show usage help
+`;
+
+function main(argv, env, { createConnection }) {
+    const cli = docopt(usage, { argv: argv.slice(2) });
+    program(createConnection({
+	host: cli['--host'], port: parseInt(cli['--port']),
+	user: cli['--user'], password: env[cli['--passkey']] }))
+	.then(() => console.log('Waiting for database events...'))
+	.catch(console.error);
+}
+
+const program = async (connection) => {
   const instance = new MySQLEvents(connection, {
     startAtEnd: true,
     excludedSchemas: {
@@ -25,11 +43,18 @@ const program = async () => {
       console.log(event);
     },
   });
-  
+
   instance.on(MySQLEvents.EVENTS.CONNECTION_ERROR, console.error);
   instance.on(MySQLEvents.EVENTS.ZONGJI_ERROR, console.error);
 };
 
-program()
-  .then(() => console.log('Waiting for database events...'))
-    .catch(console.error);
+
+if (require.main == module) {
+    // Import primitive effects only when invoked as main module.
+    //
+    // TODO: document Object capability discipline in CONTRIBUTING.md.
+    main(process.argv, process.env,
+         {
+	     createConnection: require('mysql').createConnection
+	 });
+}
