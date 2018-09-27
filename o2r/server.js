7463f2f654b397656a04fa26e57443c4a786cfb7
Dan Connolly <dckc@madmode.com>
2018-09-26 22:58:58 -0500

    o2r: airbnb style for server.js

--- o2r/server.js
+++ o2r/server.js
@@ -1,16 +1,16 @@
 /** server -- RChain / OAuth gateway
 
-See also: CONTRIBUTING.md design notes on Capper and webkeys
-as well as ocap discipline.
+    See also: CONTRIBUTING.md design notes on Capper and webkeys
+    as well as ocap discipline.
 
-ISSUE: add @flow static types
+    ISSUE: add @flow static types
 */
 
 const Capper = require('Capper');
 const docopt = require('docopt').docopt;
 const rnodeAPI = require('rchain-api');
 
-const capper_start = require('./capper_start');
+const capperStart = require('./capper_start');
 const gateway = require('./gateway/server/main');
 const keyPair = require('./gateway/server/keyPair');
 const gameSession = require('./gateway/server/gameSession');
@@ -22,142 +22,140 @@ webkey, whose state includes a key pair for use on RChain.
 Then visit that webkey URL in your browser to configure the rest.
 
 Usage:
-  server.js [options] list
-  server.js [options] make REVIVER [ARG...]
-  server.js [options] post WEBKEY METHOD [ARG...]
-  server.js [options] drop WEBKEY
-  server.js [options]
+server.js [options] list
+server.js [options] make REVIVER [ARG...]
+server.js [options] post WEBKEY METHOD [ARG...]
+server.js [options] drop WEBKEY
+server.js [options]
 
 Options:
- REVIVER                app reviver; e.g. gateway.gateway
- --conf FILE            specification of protocol (http / https), domain, and
-                        port of this service, in JSON.
-                        [default: capper.config]
- --ssl DIR              where to find SSL server.key, server.crt, ca.crt
-                        if protocol is https
-                        [default: ./ssl]
- --db FILE              persistent object storage
-                        [default: capper.db]
- --grpc-host NAME       Where to contact rnode gRPC service [default: localhost]
- --grpc-port NUM        Where to contact rnode gRPC service [default: 40401]
- -h --help              show usage
+REVIVER                app reviver; e.g. gateway.gateway
+--conf FILE            specification of protocol (http / https), domain, and
+port of this service, in JSON.
+  [default: capper.config]
+--ssl DIR              where to find SSL server.key, server.crt, ca.crt
+if protocol is https
+[default: ./ssl]
+--db FILE              persistent object storage
+[default: capper.db]
+--grpc-host NAME       Where to contact rnode gRPC service [default: localhost]
+--grpc-port NUM        Where to contact rnode gRPC service [default: 40401]
+  -h --help              show usage
 
 ISSUE: add option to list all REVIVERs?
-ISSUE: help on each REVIVER?
-`;
+  ISSUE: help on each REVIVER?
+  `;
 
 const def = obj => Object.freeze(obj);
 
 
 function main(argv, { fs, path, clock, crypto, https, express, passport, grpc }) {
-    const unique = Capper.caplib.makeUnique(crypto.randomBytes);
+  const unique = Capper.caplib.makeUnique(crypto.randomBytes);
 
-    const cli = docopt(usage, { argv: argv.slice(2) });
-    // console.log('DEBUG: cli:', cli);
+  const cli = docopt(usage, { argv: argv.slice(2) });
+  // console.log('DEBUG: cli:', cli);
 
-    const dbfile = Capper.fsSyncAccess(fs, path.join, cli['--db']);
-    const rd = arg => Capper.fsReadAccess(fs, path.join, cli[arg]);
-    const rnode = rnodeAPI.clientFactory({ grpc, clock }),
-          rchain = rnode.casperClient({
-	      host: cli['--grpc-host'],
-	      port: parseInt(cli['--grpc-port'])
-	  });
+  const dbfile = Capper.fsSyncAccess(fs, path.join, cli['--db']);
+  const rd = arg => Capper.fsReadAccess(fs, path.join, cli[arg]);
+  const rnode = rnodeAPI.clientFactory({ grpc, clock });
+  const rchain = rnode.casperClient({
+    host: cli['--grpc-host'],
+    port: parseInt(cli['--grpc-port'], 10),
+  });
 
-    Capper.makeConfig(rd('--conf')).then(config => {
-	let signIn;  // ISSUE: how to link to the oauthClient at start-up?
+  Capper.makeConfig(rd('--conf')).then((config) => {
+    let signIn; // ISSUE: how to link to the oauthClient at start-up?
 
-	const app = express(),
-	      expressWrap = () => app;
-	const apps = def({
-	    gateway: gateway.appFactory({app, passport, setSignIn,
-					 sturdyPath,
-					 baseURL: config.domain}),
-	    keyChain: keyPair.appFactory({ randomBytes: crypto.randomBytes }),
-	    game: gameSession.appFactory('game', { clock, rchain })
-	});
+    const app = express();
+    const expressWrap = () => app;
+    const apps = def({
+      gateway: gateway.appFactory({ app, passport, setSignIn, sturdyPath, baseURL: config.domain }),
+      keyChain: keyPair.appFactory({ randomBytes: crypto.randomBytes }),
+      game: gameSession.appFactory('game', { clock, rchain }),
+    });
 
-	const reviver = capper_start.makeReviver(apps),
-	      saver = Capper.makeSaver(unique, dbfile, reviver.toMaker),
-	      sturdy = Capper.makeSturdy(saver, config.domain);
+    const reviver0 = capperStart.makeReviver(apps);
+    const saver = Capper.makeSaver(unique, dbfile, reviver0.toMaker);
+    const sturdy = Capper.makeSturdy(saver, config.domain);
 
-	if (cli['list']) {
-	    Object.keys(apps).forEach(reviver => {
-		console.log(`app: ${reviver}`);
-		Object.keys(apps[reviver]).forEach(method => {
-		    console.log(`app reviver: ${reviver}.${method}`);
-		    const maker = apps[reviver][method];
+    if (cli.list) {
+      Object.keys(apps).forEach((reviver) => {
+        console.log(`app: ${reviver}`);
+        Object.keys(apps[reviver]).forEach((method) => {
+          console.log(`app reviver: ${reviver}.${method}`);
+          const maker = apps[reviver][method];
 
-		    if ('usage' in maker) {
-			console.log('args: ', maker.usage);
-		    }
-		});
-	    });
-	    return;
-	}
+          if ('usage' in maker) {
+            console.log('args: ', maker.usage);
+          }
+        });
+      });
+      return;
+    }
 
-        if (capper_start.command(cli, config, saver, sturdy)) {
-            return;
-        } else {
-	    // reserve the homepage before Capper does
-	    app.get("/", home);
+    if (!capperStart.command(cli, config, saver, sturdy)) {
+      // reserve the homepage before Capper does
+      app.get('/', home);
 
-	    Capper.run(argv, config, reviver, saver,
-		       rd('--ssl'), https.createServer, expressWrap);
+      Capper.run(argv, config, reviver0, saver,
+                 rd('--ssl'), https.createServer, expressWrap);
 
-	    console.log('server started...');
-	}
+      console.log('server started...');
+    }
 
-	// ISSUE: how to link to the oauthClient at start-up?
-	function setSignIn(path) {
-	    signIn = path;
-	}
+    // ISSUE: how to link to the oauthClient at start-up?
+    function setSignIn(it) {
+      signIn = it;
+    }
 
-	function home(req, res) {
-	    res.set('Content-Type', 'text/html');
-	    if (signIn) {
-		res.send(`<a href="${signIn}">Sign In</a>`);
-	    } else {
-		res.send(`<em>Stand by...</em>`);
-	    }
-        }
+    function home(req, res) {
+      res.set('Content-Type', 'text/html');
+      if (signIn) {
+        res.send(`<a href="${signIn}">Sign In</a>`);
+      } else {
+        res.send('<em>Stand by...</em>');
+      }
+    }
 
-	function sturdyPath(obj) {
-	    const webKey = sturdy.idToWebkey(saver.asId(obj));
-	    // ISSUE: double-check waterken / Capper docs on # -> ?
-	    return '/' + webKey.substring(config.domain.length).replace('#', '?');
-	}
-    }).done();
+    function sturdyPath(obj) {
+      const webKey = sturdy.idToWebkey(saver.asId(obj));
+      // ISSUE: double-check waterken / Capper docs on # -> ?
+      const unhash = webKey.substring(config.domain.length).replace('#', '?');
+      return `/${unhash}`;
+    }
+  }).done();
 }
 
 
-if (require.main == module) {
-    // Import primitive effects only when invoked as main module.
-    //
-    // See Object capability discipline design note in
-    // CONTRIBUTING.md.
-    main(process.argv,
-         {
-	     // Opening a file based on filename is a primitive effect.
-	     fs: require('fs'),
-	     // path.join is platform-specific and hence a primitive effect.
-	     path: require('path'),
-	     // Random number generation is primitive (typically implemented
-	     // as access to a special file, /dev/urandom).
-	     crypto: require('crypto'),
-	     // Access to the clock is primitive.
-	     clock: () => new Date(),
-	     // If node's https module followed ocap discipline, it would
-	     // have us pass in capabilities to make TCP connections.
-	     // But it doesn't, so we treat it as primitive.
-             https: require('https'),
-	     // If express followed ocap discipine, we would pass it
-	     // access to files and the network and such.
-             express: require('express'),
-	     // grpc is much like express
-	     grpc: require('grpc'),
-	     // The top-level passport strategy registry seems to be
-	     // global mutable state.
-	     // ISSUE: use passport constructors to avoid global mutable state?
-             passport: require('passport'),
-	 });
+if (require.main === module) {
+  // Import primitive effects only when invoked as main module.
+  //
+  // See Object capability discipline design note in
+  // CONTRIBUTING.md.
+  /* eslint-disable global-require */
+  main(process.argv,
+       {
+         // Opening a file based on filename is a primitive effect.
+         fs: require('fs'),
+         // path.join is platform-specific and hence a primitive effect.
+         path: require('path'),
+         // Random number generation is primitive (typically implemented
+         // as access to a special file, /dev/urandom).
+         crypto: require('crypto'),
+         // Access to the clock is primitive.
+         clock: () => new Date(),
+         // If node's https module followed ocap discipline, it would
+         // have us pass in capabilities to make TCP connections.
+         // But it doesn't, so we treat it as primitive.
+         https: require('https'),
+         // If express followed ocap discipine, we would pass it
+         // access to files and the network and such.
+         express: require('express'),
+         // grpc is much like express
+         grpc: require('grpc'),
+         // The top-level passport strategy registry seems to be
+         // global mutable state.
+         // ISSUE: use passport constructors to avoid global mutable state?
+         passport: require('passport'),
+       });
 }
